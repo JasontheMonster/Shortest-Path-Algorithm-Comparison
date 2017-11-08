3f6389ba26029a9013da5c87982aed172c9a469f
Shikun Wang <jasonwangsk@yahoo.com>
2017-11-07 20:01:25 -0500

    Finish Graph API

--- ShortestGraph/src/Graph.java
+++ ShortestGraph/src/Graph.java
@@ -7,28 +7,36 @@ import java.util.Random;
 
 /**
  * @author Jason
+ * Graph class implemented by ArrayList
+ * randomly simulate graph
  *
  */
 public class Graph {
-	private double prob;//probability of existing edge between nodes 
-	private int n; //maximun number of node
-	private ArrayList <Node>vertices;
+	private double prob;										//probability of existing edge between nodes 
+	private int n; 												//maximun number of node
+	private ArrayList <Node>vertices = new ArrayList <Node>(); 	//bag to store nodes
 	
 	
-	//constructor: randomly create a graph represented by LinkedList
+	/**
+	 * Constructor:
+	 * @param prob, probability of a edge existing
+	 * @param n: maximum number of vertices 
+	 */
 	public Graph (double prob, int n){
 		this.prob = prob;
 		this.n = n;
 	}
 	
-
+	/**
+	 * randomly generate a graph
+	 */
 	public void randomGraph(){
 		Random rand = new Random();
 		int num = rand.nextInt(this.n) + 1;
 		
 		for (int i =0; i<num; i++){
 			ArrayList <Edge> adj = new ArrayList <Edge>();
-			Node node = new Node(Integer.MAX_VALUE, adj);
+			Node node = new Node(i, Integer.MAX_VALUE, adj);
 			this.vertices.add(node);
 		}
 		
@@ -43,4 +51,22 @@ public class Graph {
 			}
 		}	
 	}
+	
+	/**
+	 * String representatino of Graph
+	 * @return list of [a, [(b,c)]] represent an edge between a and b with weigts c
+	 */
+	public String toString(){
+		StringBuilder str = new StringBuilder("");
+		for(int i = 0; i < this.vertices.size(); i++) {   
+			  str.append(vertices.get(i).toString());
+		} 
+		return str.toString();
+	}
+
+	 public static void main(String []args){
+		 Graph graph = new Graph(0.5, 10);
+		 graph.randomGraph();
+		 System.out.println(graph.toString());
+	 }
 }
