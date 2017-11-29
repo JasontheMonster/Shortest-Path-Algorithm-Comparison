24fc871ae2f9e284f56d7d313a97ee066038561f
Xinran Gu <xinrangu@hotmail.com>
2017-11-29 16:55:57 -0500

    update comments

--- Shortest-Path-Python/Bell_Ford.py
+++ Shortest-Path-Python/Bell_Ford.py
@@ -15,23 +15,33 @@ class Bellman_Ford(object):
         path = {}#create path tree
         for node in self.graph.bag:
             #set all the distance to source to be infinity
+            #for each u in set V do: d(u) <- infinity
             dist[node] = float("inf")
             #set all the nodes' parent to be None
             path[node] = None
         #set diatance to the source to 0
+        #d(s)<-0
         dist[source] = 0
 
-        #iterating |V| -1 times to relax edges
+        #iterating |V| -1 times to relax 
+        #for k = 1 to n-1 do
         for i in range(1, len(self.graph.bag)):
+            #for each v in set V do
+            #for each edge (u,v) in set ln(v) do
             for u, v, weight in self.graph.edges:
                 if dist[v] > dist[u] + weight:
+                    #d(v) = min{d(v), d(u)+l(u,v)}
                     dist[v] = dist[u] + weight
                     path[v] = u
 
         #detecting negative cycle
+        #for each v in set V do
+        #for each edge (u,v) in set ln(v) do
         for u, v, weight in self.graph.edges:
+            #if (d(v) > d(u) + l(u,v))
             if dist[v] > dist[u] + weight:
                 #if the edge is still failed to be relaxed, there is a negative cycle
+                #Output ``Negative Cycle''
                 print "Graph has negative cycle"
                 return None, None 
 
--- Shortest-Path-Python/Dijkstra.py
+++ Shortest-Path-Python/Dijkstra.py
@@ -12,33 +12,44 @@ class Dijkstra(object):
         self.graph = graph
 
     def solver(self, source):
+        #create prioirty queue
+        #Q<-makePQ()
+        priorityQ = []
         #set distance from source to all the node as infinity
+        #for each node u that is not s do: insert(Q,(u,infinity))
         dist = {x: float("inf") for x in self.graph.bag}
         #set distance to source to be 0
+        #insert(Q,(s,0))
         dist[source] = 0
-        #use visisted to keep track of nodes we visited 
-        visited = []
-        #create prioirty queue
-        priorityQ = []
-        #create a path tree
-        path = {}
-
         #push all the nodes to priority queue and set the parent of them as None
         for node in self.graph.bag:
             heappush(priorityQ, (dist[node], node))
             path[node] = None
+        #use visisted to keep track of nodes we visited 
+        #X<-emptyset
+        visited = []
+        #create a path tree to resturn in the end
+        path = {}
+        
+       
 
+        
         #while queue is not empty
+        #for i = 1 to |V| do
         while priorityQ:
             #extract node with minimum distance value
+            #(v, dist(s, v)) = extractMin(Q)
             d, node = heappop(priorityQ)
             #add this node to visited set
+            #X = X U {v}
             visited.append(node)
             #loop through neighbors of the current node
+            #for each u in Adj(v) do
             for neighbor, weight in self.graph.adjacent[node]:
                 #if neighbor has not been visited
                 if neighbor not in visited: 
                     #relax the edge 
+                    #decreaseKey(Q,(u,min(dist(s,u),dist(s,v)+l(v,u))))
                     if dist[neighbor] > d + weight:
                         dist[neighbor] = d + weight
                         #set the parent of neighbor to be node
